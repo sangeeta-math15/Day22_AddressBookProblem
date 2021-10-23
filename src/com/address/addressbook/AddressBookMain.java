74fb593f5bce63f0cfe689417704bdfaf6f35d53
sngtmth <sngtmth@gmail.com>
2021-10-23 23:44:14 +0530

    Added:Create a Contact Details in AddressBook

--- src/com/address/addressbook/AddressBookMain.java
+++ src/com/address/addressbook/AddressBookMain.java
@@ -3,5 +3,14 @@ package com.address.addressbook;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to AddressBook Program in AddressBookMain");
+        CreateContact createContact=new CreateContact();
+        createContact.setFirstName("Sangeeta");
+        createContact.setLastName("Math");
+        createContact.setAddressCity("Banglore");
+        createContact.setEmail("sngtmth@gamil.com");
+        createContact.setState("Karnataka");
+        createContact.setZip(123434);
+        createContact.setPhoneNumber(7865345623L);
+        System.out.println("Create Contact  " +createContact);
     }
 }

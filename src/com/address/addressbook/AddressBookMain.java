7dd0e0b6643350391aa2abe9c960fa4e3508bfe0
sngtmth <sngtmth@gmail.com>
2021-10-23 23:18:23 +0530

    Added:Welcome Message

---
+++ src/com/address/addressbook/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.address.addressbook;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to AddressBook Program in AddressBookMain");
+    }
+}
